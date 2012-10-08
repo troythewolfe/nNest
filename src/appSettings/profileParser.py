c89a0c7d4a4bf16ac379641c57b476ccd5e4dca9
troythewolfe <wolfestuff@gmail.com>
2012-10-07 23:14:26 -0700

    removed noise from profileParser, moved to server

--- src/appSettings/profileParser.py
+++ src/appSettings/profileParser.py
@@ -1,14 +1,4 @@
-import profileList
-from ua_parser import user_agent_parser
-
-def init(request, pathParams={}):
-	print 'profileParser'
-
-
-	profiles = profileList.init()
-	uaString = request.headers.get('User-Agent')
-	ua = user_agent_parser.Parse(uaString)
-                                 
+def init(profiles, ua, request, pathParams={}):                              
 	#default
 	profileName = profiles['modernWeb']
 	
@@ -20,11 +10,4 @@ def init(request, pathParams={}):
 	if ua['user_agent']['family'] == "Chrome":
 		profileName = profiles['chromeWeb']
 
-	profile = {
-		'name' : profileName,
-		'ua' : ua,
-		'path' : request.path,
-		'pathParams' : pathParams
-	}
-
-	return profile
+	return profileName
