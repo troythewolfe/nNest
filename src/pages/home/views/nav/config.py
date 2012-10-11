329bbf96513eb53b0d67d5df03a1f0de71a98f13
troythewolfe <wolfestuff@gmail.com>
2012-10-11 08:05:56 -0700

    adding views into pages

---
+++ src/pages/home/views/nav/config.py
@@ -0,0 +1,25 @@
+import util.inc as inc
+from util.configView import ConfigView
+import os
+
+#nav
+def init(profile):
+	config = ConfigView(os.path.abspath(__file__))
+
+	config.jsInc = [
+		config.get('js')
+	]
+
+	config.templates = [
+		config.get('template', 'navItem'),
+		config.get('template', 'listItem', 'shared'),
+		config.get('template', 'listItem1', 'shared'),
+		config.get('template', 'main', 'buttons')
+	]
+
+	import views.footer.config as footer
+	config.views = {
+		'footer' : footer.init(profile)
+	}
+
+	return config
