de5db97a6650ce1e3e2aaf1ac46b7fc9077c81dd
troythewolfe <wolfestuff@gmail.com>
2012-11-02 09:46:00 -0700

    building json dependency infrastructure

--- src/util/parseConfigJson.py
+++ src/util/parseConfigJson.py
@@ -21,7 +21,7 @@ def parseConfig(loc):
 			path = 'pages/' + loc['page'] + '/view/' + loc['view']
 		elif not ('page' in loc) and ('view' in loc):
 			path = 'views/' + loc['view']
-			
+
 		buildPath = {
 			'path' : path,
 			'profile' : configType
@@ -45,25 +45,46 @@ def parseConfig(loc):
 		error.log('badly formed json in ' + configPath + '.json')
 		return False
 
-	for js in config_json['js']:
-		print 'js---'
-		print type(js)
-		print js
-		if type(js) is unicode:
-			print 'is string ----'
-			print js
-
+	assetType = 'js'
+	if assetType == 'js':
+		for asset in config_json[assetType]:
+			assetPath = {
+				'location' : 'internal',
+				'path' : False
+			}
+			
+			if type(asset) is unicode:
+				assetPath['path'] = buildPath['path'] + '/' + assetType + '/' + asset + '.' + assetType
+			elif type(asset) is dict:
+				if ('page' in asset) and ('view' in asset):
+					assetPath['path'] = 'pages/' + asset['page'] + '/views/' + asset['view'] + '/' + assetType + '/' + asset[assetType] + '.' + assetType
+				elif('page' in asset):
+					assetPath['path'] = 'pages/' + asset['page'] + '/' + assetType + '/' + asset[assetType] + '.' + assetType
+				elif('view' in asset):
+					assetPath['path'] = 'views/' + asset['view'] + '/' + assetType + '/' + asset[assetType] + '.' + assetType
+				elif('js' in asset):
+					assetPath['path'] = assetType + '/' + asset[assetType] + '.' + assetType
+				elif('ext' in asset):
+					pass
+					assetPath['path'] = asset['ext']
+					assetPath['location'] = 'external'
+			
+			if assetPath['path'] is False:
+				error.log('badly formed' + assetType + ' include in ' + configPath)
+		
 	return config_json
 
 
-print 'page:home'
-print '-------------------'
-print parseConfig({
+#print 'page:home'
+#print '-------------------'
+currConfig = parseConfig({
 	'profile' : 'chromeWeb',
 	'page' : 'home'
 })
-print '-------------------'
-print ' '
+
+print currConfig
+#print '-------------------'
+#print ' '
 
 """
 print 'page:home, view:nav'
@@ -72,6 +93,7 @@ print parseConfig({
 	'page' : 'home',
 	'view' : 'nav'
 })
+
 print '-------------------'
 print ' '
 
